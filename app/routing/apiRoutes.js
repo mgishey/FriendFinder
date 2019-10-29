2eb457bd1cb15fa8a4cf7eddd8a2bf07a82b7135
Mike Gishey <mgishey56@gmail.com>
2019-10-29 16:25:23 -0700

    html routes working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,36 @@
-// We are inking our routes to a series of data sources
-// This data source holds an array of friends
+//Linking to the array of friends
 
 var friendsData = require("../data/friends");
 
 // ROUTING
 module.exports = function (app) {
+
     // When a user visits (localhost:8080/api/friends)
     // they are shown a JSON of the data in friends.js
+
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    // This handles when a user submits the form to the server.
-    // The JSON  is pushed  to the friendsArray
+    
+
     app.post("/api/friends", function (req, res) {
-        friendsData.push(req.body);
+        var newFriend = req.body
+        console.log("New Friend: " + JSON.stringify(newFriend));
+
+        // Add a new friend to friends array
+        friendsData.push(newFriend);
+
+        // Get new friend scores
+         var newScores = newFriend.scores;
+        console.log("User Scores: " + newScores);
 
         // Compatiblity code goes here and is then sent back
         // to the user on the survey.html page
+        
+    
 
 
 
-        res.json(true);
     });
 }
