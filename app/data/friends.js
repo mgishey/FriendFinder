c202280ba53208028b50b2b59edb9faf6dc11859
Mike Gishey <mgishey56@gmail.com>
2019-10-30 17:13:18 -0700

    all routes running

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,40 @@ var friendArray = [
             2,
             5,
             5,
-            1
+            1,
+            2
+        ]
+    },
+    {
+        name: "Tree Girl",
+        photo: "https://www.goodnet.org/photos/620x0/24888.jpg",
+        scores: [
+            5,
+            3,
+            1,
+            2,
+            5,
+            5,
+            3,
+            1,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Plain Jane",
+        photo: "https://www.istockphoto.com/photo/young-woman-in-a-blank-gray-shirt-gm119828531-15116797",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 //Linking to the array of friends
 
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 
 // ROUTING
 module.exports = function (app) {
@@ -12,25 +12,56 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    
+
 
     app.post("/api/friends", function (req, res) {
         var newFriend = req.body
         console.log("New Friend: " + JSON.stringify(newFriend));
 
-        // Add a new friend to friends array
-        friendsData.push(newFriend);
-
         // Get new friend scores
-         var newScores = newFriend.scores;
-        console.log("User Scores: " + newScores);
+        var newScores = newFriend.scores;
+        console.log("New Scores: " + newScores);
 
         // Compatiblity code goes here and is then sent back
         // to the user on the survey.html page
-        
-    
+
+
+        // Set fakeDiff high so that the first entry's values are read
+        // and subsequent values are compared to the preceding diff value.
+        fakeDiff = 1000;
+
+        // Loop through existing friends
+        for (var i = 0; i < friendsData.length; i++) {
+            var diff = 0;
+
+            // Loop through new friend
+            for (var j = 0; j < newScores.length; j++) {
+                diff += Math.abs(friendsData[i].scores[j] - newScores[j]);
+                // console.log("OF Score: " + friendsData[i].scores[j] +  "------ NF Score: " + newScores[j]);
+                // console.log("diff: " + diff);
+            }
+            // console.log("Each score difference: " + diff);
+
+            if (diff < fakeDiff) {
+                fakeDiff = diff;
+                matchName = friendsData[i].name;
+                matchPhoto = friendsData[i].photo;
+
+            }
+        }
+        // console.log("Match Name: " + matchName);
+        // console.log("Match Photo: " + matchPhoto);
+
+        res.json({ name: matchName, photo: matchPhoto });
+
+        // Add new friend
+
+        friendsData.push(newFriend);
+
+
+
 
 
 
     });
-}
+};
